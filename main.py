7fb7a1037d8ad5244e6fc3880b01f6e91681f2fc
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2015-11-29 13:39:24 +0200

    save old partly working formula

--- main.py
+++ main.py
@@ -1,65 +1,122 @@
 #!bin/python
 # -*- coding: utf-8 -*-
+from __future__ import division
 
 import numpy as np
 import cv2
 from random import randint
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 win_header = 'view window'
 cv2.namedWindow(win_header, cv2.WINDOW_NORMAL)
 
-def process(frame, mask, rows, cols):
+def show(img):
+    #print(img.shape)
+    cv2.imshow(win_header, img)
 
-    # Apply MASK.
+    k = cv2.waitKey(0) & 0xFF
+
+    if k == 27:     # ESC key
+        cv2.destroyWindow(win_header)
+
+def show_plt(img):
+    plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+    #plt.xticks([]), plt.yticks([])
+    plt.show()
+
+def create_masks(height, width):
+    '''Creates two mask shapes
+    Args:
+        height: original image HEIGHT
+        width: original image WIDTH
+    Return:
+        mask_top_bottom: top and bottom rectangles mask
+        mask_left_right: left and right rectangles mask
+    '''
+    # Scaled up 200%
+    height_2x = height
+    width_2x = width
+
+    # Set TOP_BOTTOM mask points
+    top_pt = [0.5 * width_2x, 0]
+    left_pt = [- (0.5 * width_2x), height_2x]
+    right_pt = [width_2x*1.5, height_2x]
+    top_bottom_mask = mask(height_2x, width_2x, top_pt, left_pt, right_pt)
+    #show(top_bottom_mask)
+    # Set TOP_BOTTOM mask points
+    top_pt = [0.5 * width_2x, 0]
+    left_pt = [width_2x, -1/3 * height_2x]
+    right_pt = [width_2x, 5/3 * height_2x]
+    left_right_mask = mask(height_2x, width_2x, top_pt, left_pt, right_pt)
+    #show(left_right_mask)
+    return top_bottom_mask, left_right_mask
+
+def mask(height_2x, width_2x, top_pt, left_pt, right_pt):
+    '''Creates triangle mask from 3x points
+    Args:
+        top_pt
+        left_pt
+        right_pt
+    Return:
+        mask
+    '''
+    print(top_pt, left_pt, right_pt)
+    mask = np.zeros((height_2x, width_2x, 3), np.uint8)
+    show_plt(mask)
+    pts = np.array([top_pt, left_pt, right_pt], np.int32)
+    #pts = pts.reshape((-1,1,2))
+    mask = cv2.fillConvexPoly(mask, pts, (255,255,255), 1)
+    show_plt(mask)
+    mask2grey = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(mask2grey, 10, 255, cv2.THRESH_BINARY)
+    show(mask)
+    return mask
+
+def crop_triangles(frame, mask, height, width):
+    '''Crop triangels by mask
+    '''
+    # Apply MASK. ???????
+
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # Scale 
+    # Scale down 
+    #print(height, width)
+    result = cv2.resize(result, (height//2, width//2), interpolation = cv2.INTER_CUBIC)
 
     # Rotate image 90 CCW
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
-    result = cv2.warpAffine(result,M,(cols,rows))
+    #M = cv2.getRotationMatrix2D((width/2,height/2),90,1)
+    #result = cv2.warpAffine(result,M,(width,height))
 
     return result
 
 
 cap = cv2.VideoCapture(0)
-
+# Get image dimensions ( HIGH x WIDTH )
+# 480 x 640
+height = int(cap.get(4))
+width = int(cap.get(3))
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+# CREATE MASKs 
+top_bottom_mask, left_right_mask = create_masks(height, width)
+
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
-
-        rows, cols = frame.shape[:2]
-        print(rows, cols)
-
-        ###### CREATE MASK ######
-        # Create a black image
-        img = np.zeros((rows, cols, 3), np.uint8)
-
-        p1 = [cols/2, 0]
-        p2 = [0, rows]
-        p3 = [cols, rows]
-        pts = np.array([p1,p2,p3], np.int32)
-        #pts = pts.reshape((-1,1,2))
-
-        mask = cv2.fillConvexPoly(img, pts, (255,255,255), 1)
-        #show(mask)
-        mask2gray = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
-        
-        ret, mask = cv2.threshold(mask2gray, 10, 255, cv2.THRESH_BINARY)
-
-        frame = process(frame, mask, rows, cols)
+ 
+        top_bottom = crop_triangles(frame, top_bottom_mask, height, width)
+        left_right = crop_triangles(frame, left_right_mask, height, width)
 
         #frame = cv2.flip(frame,0)
 
         # write the frame
-        out.write(frame)
+        #out.write(frame)
 
-        cv2.imshow(win_header,frame)
+        #cv2.imshow(win_header,left_right)
+        #cv2.imshow(win_header,frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
@@ -67,5 +124,5 @@ while(cap.isOpened()):
 
 # Release everything if job is finished
 cap.release()
-out.release()
+#out.release()
 cv2.destroyAllWindows()
---
+++ test.py
@@ -0,0 +1,109 @@
+#!bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+import numpy as np
+import cv2
+from random import randint
+from matplotlib import pyplot as plt
+
+
+def show(img):
+    cv2.imshow(win_header, img)
+
+    k = cv2.waitKey(0) & 0xFF
+
+    if k == 27:     # ESC key
+        cv2.destroyWindow(win_header)
+
+def show_plt(img):
+    plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+    #plt.xticks([]), plt.yticks([])
+    plt.show()
+
+def create_masks(height, width):
+    '''Creates two mask shapes
+    Args:
+        height: original image HEIGHT
+        width: original image WIDTH
+    Return:
+        mask_top_bottom: top and bottom rectangles mask
+        mask_left_right: left and right rectangles mask
+    '''
+    # Set TOP_BOTTOM mask points
+    top_pt = [width / 2, 0]
+    left_pt = [- (width / 2), height]
+    right_pt = [width * 1.5, height]
+    pts = np.array([top_pt, left_pt, right_pt], np.int32)
+    top_bottom_mask = mask(height, width, pts)
+
+    # Set TOP_BOTTOM mask points
+    top_pt = [width / 2 , 0]
+    left_pt = [width - height, width]    #????!!!!
+    right_pt = [(width - height) + height * 2, width]   #????!!!!
+    pts = np.array([top_pt, left_pt, right_pt], np.int32)
+    left_right_mask = mask(height, width, pts)
+
+    return top_bottom_mask, left_right_mask
+
+def mask(height, width, pts):
+    '''Creates triangle mask from 3x points
+    Args:
+        top_pt
+        left_pt
+        right_pt
+    Return:
+        mask
+    '''
+    print(pts)
+    mask = np.zeros((height, width, 3), np.uint8)
+    #pts = pts.reshape((-1,1,2))
+    mask = cv2.fillConvexPoly(mask, pts, (255,255,255), 1)
+    
+    mask2grey = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(mask2grey, 10, 255, cv2.THRESH_BINARY)
+    #show(mask)
+    return mask
+
+def crop_triangles(img, mask):
+    '''Crop triangels by mask
+    '''
+    return cv2.bitwise_and(img, img, mask=mask)
+
+def scale(img, scalar):
+    height, width = img.shape[:2]
+    result = cv2.resize(img, (int(width/scalar), int(height/scalar)), \
+                        interpolation = cv2.INTER_CUBIC)
+    return result
+
+def rotate(img, angle):
+    '''Rotates image about its centre
+    '''
+    height, width = img.shape[:2]
+    M = cv2.getRotationMatrix2D((width, height), angle, 1)
+    result = cv2.warpAffine(result, M, (width, height))
+    return result
+
+
+win_header = 'view window'
+cv2.namedWindow(win_header, cv2.WINDOW_NORMAL)
+
+img = cv2.imread('aero3.jpg', 0)
+#img = cv2.imread('lena.jpg', 0)
+# Get image dimensions ( HIGH x WIDTH )
+# 480 x 640
+height, width = img.shape
+
+# CREATE MASKs 
+top_bottom_mask, left_right_mask = create_masks(height, width)
+
+ 
+top_bottom = crop_triangles(img, top_bottom_mask)
+left_right = crop_triangles(img, left_right_mask)
+
+show(img)
+show(top_bottom)
+show(left_right)
+
+if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
